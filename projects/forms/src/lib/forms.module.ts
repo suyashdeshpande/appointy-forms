0aa044e57d53ed872cc6ca3a70557c30076deff1
Suyash <suyash.champ1@gmail.com>
2018-08-07 22:19:56 +0530

    chore: initial commit

---
+++ projects/forms/src/lib/forms.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormsComponent } from './forms.component';
+
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [FormsComponent],
+  exports: [FormsComponent]
+})
+export class FormsModule { }
